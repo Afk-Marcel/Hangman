41228114fd3097501eb0d6565156f97dc6bf91ae
afk-Marcel <mjgreyling@icloud.com>
2024-08-03 21:16:32 +0200

    Revert "Update"
    This reverts commit 801e72e5dbf89c790a3d4bb3d1a39160a96c3305.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   // Fetch words from Dictionary.txt on component mount
   useEffect(() => {
-    fetch("/dictionary.txt")
+    fetch("/Dictionary.txt")
       .then((response) => response.text())
       .then((text) => {
         const wordsArray = text
